4ab15db7820bee1cfd19548c148145bd9ad16f4b
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2018-10-05 16:27:57 +0100

    Improved source commenting

--- framestatsbar.py
+++ framestatsbar.py
@@ -1,11 +1,26 @@
+"""Frame statistics bar widget for live viewer.
+
+This class implememnts a PyQT widget for displaying frame statistics
+in the live viewer application.
+
+Tim Nicholls, STFC Application Engineering Group.
+"""
 from PyQt5 import QtWidgets
 
 class FrameStatisticsBar(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
+        """
+        Frame statistics bar widget for live viewer.
 
+        This class implememnts a PyQT widget for displaying frame statistics
+        in the live viewer application.
+        """
+
+        # Initialise super class
         super(FrameStatisticsBar, self).__init__(parent)
 
+        # Create box layout and widgets
         hbl = QtWidgets.QHBoxLayout(self)
         self.label_recvd = QtWidgets.QLabel("Frames recvd:")
         self.frames_recvd = QtWidgets.QLabel("0")
@@ -14,7 +29,8 @@ class FrameStatisticsBar(QtWidgets.QWidget):
         self.frames_shown = QtWidgets.QLabel("0")
 
         self.reset_button = QtWidgets.QPushButton("Reset")
-
+    
+        # Pack widgets into layout
         hbl.addWidget(self.label_recvd)
         hbl.addWidget(self.frames_recvd)
         hbl.addWidget(self.label_shown)
@@ -23,11 +39,12 @@ class FrameStatisticsBar(QtWidgets.QWidget):
         hbl.addWidget(self.reset_button)
 
     def update(self, frames_recvd, frames_shown):
-
+        """Update the counters and repaint."""
         self.frames_recvd.setText(str(frames_recvd))
         self.frames_recvd.repaint()
         self.frames_shown.setText(str(frames_shown))
         self.frames_shown.repaint()
         
     def connect_reset(self, slot):
+        """Connect the reset button to the specified slot."""
         self.reset_button.clicked.connect(slot)
--- mplplot.py
+++ mplplot.py
@@ -1,3 +1,11 @@
+"""Matplotlib-based 2D image canvas widget for ODIN data viewer.
+
+This class implements a Matplotlib 2D image plotting canvas for use in the odin-data
+live viewer. It supports efficient re-rendering of incoming images to maximise
+performance.
+
+Tim Nicholls, STFC Application Engineering Group.
+"""
 import numpy as np
 
 import matplotlib
@@ -13,12 +21,22 @@ from matplotlib.figure import Figure
 from matplotlib.colorbar import make_axes, Colorbar
 
 class MplPlotCanvas(FigureCanvas):
- 
+    """
+    Matplot plot canvas.
+
+    This class implements the updating 2D image canvas.
+    """
     matplotlib.rcParams.update({'font.size': 8})
     cbar_numticks = 9
 
     def __init__(self, parent=None, dpi=100):
+        """
+        Initialise the plot canvas object.
 
+        This method initialises the plot canvas object, creating an empty subplot
+        within it.
+        """
+        # Create the figure canvas
         self.figure = Figure(dpi=dpi)
         FigureCanvas.__init__(self, self.figure)
         self.setParent(parent)
@@ -27,21 +45,30 @@ class MplPlotCanvas(FigureCanvas):
                 QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
         
+        # Add the image axes to the figure
         self.axes = self.figure.add_subplot(111)
         self.axes.set_xticks([])
         self.axes.set_yticks([])
 
+        # Set up storage variables
         self.img_range = ()
         self.img_shape = None
         self.img_obj = None
         self.colorbar = None
         self.bar_orient = 'horizontal'
 
+        # Render an initial empty image frame
         self.render_frame(np.zeros((10, 10), dtype=np.uint16), min_val=0, max_val=10)
         self.figure.tight_layout()
 
     def render_frame(self, img_data, min_val=None, max_val=None):
+        """
+        Render an image frame in the plot canvas.
 
+        This method renders a scaled image and colorbar in the plot canvas. For speed,
+        the image data is just updated if there is no change to the image shape, otherwise
+        the axes and colorbar are redrawn.
+        """
         # If the minimum and/or maximum values are not defined, determine from the 
         # incoming image data
         if min_val is None:
@@ -54,11 +81,14 @@ class MplPlotCanvas(FigureCanvas):
         if self.img_shape != img_data.shape:
             self.img_shape = img_data.shape
             self.img_obj = None
+
+            # Set the colorbar orientation dependent on the aspect ratio of the image
             if self.img_shape[0] < self.img_shape[1]:
                 self.bar_orient = 'horizontal'
             else:
                 self.bar_orient = 'vertical'
 
+            # Remove any existing colorbar prior to subequent re-draw
             if self.colorbar is not None:
                 self.colorbar.remove()
                 self.colorbar = None
@@ -91,7 +121,11 @@ class MplPlotCanvas(FigureCanvas):
         self.draw()
 
 class MplNavigationToolbar(NavigationToolbar):
+    """
+    Navigation toolbar for the plot canvas.
 
+    This class is a simple wrapper around the imported navigation toolbar.
+    """
     def __init__(self, canvas, parent, coordinates=True):
-
+        """Initialise the toolbar."""
         super(MplNavigationToolbar, self).__init__(canvas, parent, coordinates)
